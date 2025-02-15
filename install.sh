d6a299d234ca1895a105723bcbac2e2ccf08769f
anttiharju <git@anttiharju.dev>
2025-02-15 10:56:18 +0200

    Remove unnecessary argument

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -eu
 goversion="$1"
 goos="$2"
 goarch="$3"
-path="$4"
 
+path="$HOME/.vmatch/go/v${goversion}"
 mkdir -p "$path"
 
 url="https://go.dev/dl/go${goversion}.${goos}-${goarch}.tar.gz"
