9d44800d1c65251505f5e16ee4bd23dca63b163f
anttiharju <git@anttiharju.dev>
2025-02-15 10:51:55 +0200

    Revert "Turns out it needs to be 2"
    This reverts commit 16d867afc9fef6a30d8091a820d8de2f2de95559.

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ goos="$3"
 goarch="$4"
 
 url="https://go.dev/dl/${goversion}.${goos}-${goarch}.tar.gz"
-curl -sL "$url" | tar -C "$path" --strip-components=2 -xz
+curl -sL "$url" | tar -C "$path" --strip-components=1 -xz
